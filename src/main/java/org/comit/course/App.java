6f4ec83670b2b20ccdd29632b26c793f32fee1a2
Snizhana Kshetska <skshetska@gmail.com>
2023-05-31 22:28:21 -0600

    First commit

---
+++ src/main/java/org/comit/course/App.java
@@ -0,0 +1,13 @@
+package org.comit.course;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/comit/course/_01_variables/Variables.java
@@ -0,0 +1,55 @@
+package org.comit.course._01_variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 Comments:
+		 1.variable declaration
+		 2.variable initialization
+		 We have two types of variables:
+		 1. primitives
+		 2. class variables
+		 */
+		/*
+		 * Integer Variables default int: 98%  long: 2%
+		 */
+				int num1 = 2147483647;
+				long num2 = 2147483648L;
+				byte num3 = 10;
+				short num4 = 30000;
+				int Num4 = 4;
+				
+				/*
+				 * decimal Variables
+				 */
+				double num5 = 4.3; // Default: 99.9999%
+				double exchangeRate = 5.4; // Camel case
+				float num6 = 4.3F;
+				/*
+				 * Text Variables
+				 */
+				char char1 = 'A'; // only 1 character 
+				String str1 = "Hello, World!"; // this use most of the time
+				/*
+				 * Boolean Variables
+				 */
+				boolean boo1 = true;
+				boolean boo2 = false;
+				
+				/*
+				 * Class Variables
+				 */
+				Integer num11 = 2147483647;
+				Long num21 = 2147483648L;
+				Byte num31 = 10;
+				Short num41 = 30000;
+				Double num51 = 4.3;
+				Character  char11= 'a';
+				
+				System.out.println("The value is: " + char1);
+
+	}
+
+}
